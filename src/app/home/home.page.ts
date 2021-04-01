1506b9d4dd6d17676a618b4aea793d0dc5562bec
Edwin <edd.joy01@gmail.com>
2021-04-01 23:12:37 +0530

    Integrated maps api & deplyoed dummy markers

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,76 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  map: any;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+  
+  infoWindows: any = [];
+  markers: any = [
+    {
+      title: "Vettikkattiri Bus Stop",
+      latitude: "10.731151996842799",
+      longitude: "76.28255923660738"
+    },
+    {
+      title: "Vetti Stop 2",
+      latitude: "10.727894391125544",
+      longitude: "76.28247438539123"
+    }
+  ];
+
   constructor() {}
 
+  ionViewDidEnter() {
+    this.showMap();
+  }
+
+  addMarkersToMap(markers) {
+    for (let marker of markers) {
+      let position = new google.maps.LatLng(marker.latitude, marker.longitude);
+      let mapMarker = new google.maps.Marker({
+        position: position,
+        title: marker.title,
+        latitude: marker.latitude,
+        longitude: marker.longitude
+      });
+      mapMarker.setMap(this.map);
+      this.addInfoWindowToMarker(mapMarker);
+    }
+  }
+
+  addInfoWindowToMarker(marker) {
+    let infoWindowContent = '<div id="content">' +
+                              '<h2 id="firstHeading" class"firstHeading">' + marker.title + '</h2>' +
+                              '<p>Latitude: ' + marker.latitude + '</p>' +
+                              '<p>Longitude: ' + marker.longitude + '</p>' +
+                              '</div>';
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: infoWindowContent
+    });
+
+    marker.addListener('click', () => {
+      this.closeAllInfoWindows();
+      infoWindow.open(this.map, marker);
+    });
+    this.infoWindows.push(infoWindow);
+  }
+
+  closeAllInfoWindows() {
+    for(let window of this.infoWindows) {
+      window.close();
+    }
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(10.72815116035569, 76.28984519824705);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+      }
+      this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+      this.addMarkersToMap(this.markers);
+  }
 }
