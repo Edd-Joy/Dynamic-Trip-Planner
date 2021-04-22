0eae9d09f84ebc7085faeaabad925e340f744e40
Edwin <edd.joy01@gmail.com>
2021-04-23 00:18:18 +0530

    Added Bottom Drawer to display bus details

---
+++ src/app/components/shared-components.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DrawerComponent } from './drawer/drawer.component';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [DrawerComponent],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+  exports: [DrawerComponent]
+})
+export class SharedComponentsModule { }
